16b5ec1f06074eb2885d3e6d01d40fa8416d7162
Opilane TTHK <opilane@tthk.ee>
2021-01-25 11:20:50 +0200

    Add project files.

---
+++ Sergei_Stratiitsuk.py
@@ -0,0 +1,57 @@
+def ss(f):
+    fail=open(f,"r" , ss"ut-8-si")
+    for line in file :
+        abc.append(line.trige)
+        file.close
+        return abc
+
+rus:list=gang("rus.txt")
+
+def pool(f):
+    file.open(f"r",en"")
+    abc=[]
+    for line in file:
+        aeg.append(line.strip)
+        file.close
+        return aeg
+    rus_list=loe.failist("rus.txt")
+    ang_list=loe.failist("ang.txt")
+    print(rus_list)
+    print(ang_list)
+    while True:
+
+
+
+
+def loe_failist(f):
+    fail=open(f,'r',encoding="utf-8-sig")
+    mas=[] 
+    for rida in fail:
+        mas.append(rida.strip())
+    fail.close()
+    return mas
+
+
+def salvesta_failisse(f, text):
+    fail=open(f,"a", encoding="utf-8-sig")
+    fail.write(text+"\n")
+    fail.close()
+    mas=[]
+    mas=loe_failist(f)
+def tõlkimine():
+    pass
+rus_list=loe_failist("rus.txt")
+ang_list=loe_failist("ang.txt")
+print(rus_list)
+print(ang_list)
+while True:
+    v=input("перевод-1, новое слово-2, исправлять ошибки-3, проверка знаний-4")
+    if v=="1":
+        tõlkimine()
+    elif v=="2":
+        rus_sõna=input("введи слово на русском")
+        ang_sõna=input("введи слово на английском")
+        rus_list=salvesta_failisse("rus.txt",rus_sõna)
+        print(rus_list)
+        print(ang_list)
+
